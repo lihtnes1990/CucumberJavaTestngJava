e3eda4a886bd5b1af482ec144d5a095ed7e18104
SenthilN <senthiln@nextgen.com>
2024-06-10 17:32:07 +0530

    first commit

---
+++ src/test/java/features/homepage.feature
@@ -0,0 +1,6 @@
+Feature: Home Page
+  Scenario: Verify the home page
+    Given I load the page
+    When I see the page
+    And I click on the link
+    Then I verify the option
---
+++ src/test/java/stepdef/homepageStepdefs.java
@@ -0,0 +1,29 @@
+package stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class homepageStepdefs {
+    @Given("I load the page")
+    public void i_load_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("load page");
+    }
+    @When("I see the page")
+    public void i_see_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("load page");
+    }
+    @When("I click on the link")
+    public void i_click_on_the_link() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("load page");
+    }
+    @Then("I verify the option")
+    public void i_verify_the_option() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("load page");
+    }
+}
